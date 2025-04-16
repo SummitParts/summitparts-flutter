19f42e8eaf98bfd584fa9b8e8e540cb657e64446
Khaleel Shaheen <khaleel_sh@hotmail.com>
2025-04-16 17:43:49 +0300

    Add light app theme

---
+++ lib/core/style/light_app_theme.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:summit_parts/core/app/constants.dart';
+import 'package:summit_parts/core/style/app_theme.dart';
+
+final ThemeData lightTheme = _LightAppTheme().theme();
+
+@immutable
+class _LightAppTheme extends AppTheme {
+  @override
+  ThemeData theme() {
+    final colorScheme = ColorScheme.fromSeed(
+      brightness: Brightness.light,
+      seedColor: const Color(0xFF0C71C3),
+      surface: const Color(0xFFF8F8F8),
+      onSurface: const Color(0xFF171717),
+    );
+
+    return ThemeData(fontFamily: Constants.poppinsFontFamily, brightness: Brightness.light, colorScheme: colorScheme);
+  }
+}
