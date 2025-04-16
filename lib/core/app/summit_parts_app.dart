5ebb9eba581d16ba943eb9d0bc34dc64c24e2deb
Khaleel Shaheen <khaleel_sh@hotmail.com>
2025-04-16 17:44:52 +0300

    Update SummitPartsApp configs

--- lib/core/app/summit_parts_app.dart
+++ lib/core/app/summit_parts_app.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:summit_parts/core/app/router.dart';
+import 'package:summit_parts/core/style/light_app_theme.dart';
+import 'package:summit_parts/core/ui/widget/generic_error.dart';
 
 class SummitPartsApp extends StatelessWidget {
   const SummitPartsApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Summit Parts', home: const Scaffold());
+    return MaterialApp.router(
+      title: 'Summit Parts',
+      theme: lightTheme,
+      builder: (context, child) {
+        ErrorWidget.builder = (error) => GenericError(exception: Exception(error.exception));
+        return child!;
+      },
+      routerConfig: router,
+    );
   }
 }
