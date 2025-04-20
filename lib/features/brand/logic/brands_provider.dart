7ebf91027fe91946bb6ae8e215de29fe721f1b4d
Khaleel Shaheen <khaleel_sh@hotmail.com>
2025-04-20 22:24:04 +0300

    Add brands and parts future providers

---
+++ lib/features/brand/logic/brands_provider.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:summit_parts/features/brand/data/brands_data_provider.dart';
+import 'package:summit_parts/features/brand/model/brand.dart';
+
+final featuredBrandsProvider = FutureProvider<List<Brand>>((ref) async {
+  return ref.read(brandsDataProvider).getFeaturedBrands();
+});
