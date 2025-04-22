a0dca068300c6438cde18405d7ac89dcaa43c182
Khaleel Shaheen <khaleel_sh@hotmail.com>
2025-04-22 12:01:55 +0300

    Add brandsProvider

--- lib/features/brand/logic/brands_provider.dart
+++ lib/features/brand/logic/brands_provider.dart
@@ -2,6 +2,10 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:summit_parts/features/brand/data/brands_data_provider.dart';
 import 'package:summit_parts/features/brand/model/brand.dart';
 
-final featuredBrandsProvider = FutureProvider<List<Brand>>((ref) async {
+final featuredBrandsProvider = FutureProvider.autoDispose<List<Brand>>((ref) async {
   return ref.read(brandsDataProvider).getFeaturedBrands();
+});
+
+final brandsProvider = FutureProvider.autoDispose<List<Brand>>((ref) async {
+  return ref.read(brandsDataProvider).getAllBrands();
 });
