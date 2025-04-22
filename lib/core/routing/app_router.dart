52bfb4fa93948dacbbd71b630d82d714ebe68b97
Khaleel Shaheen <khaleel_sh@hotmail.com>
2025-04-22 10:48:15 +0300

    Add BrandsListScreen path to the router

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,10 +1,14 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:summit_parts/features/brand/ui/screen/brands_list_screen.dart';
 import 'package:summit_parts/features/home_navigation/ui/screen/home_navigation_screen.dart';
 
 final routerProvider = Provider<GoRouter>((_) {
   return GoRouter(
     initialLocation: HomeNavigationScreen.path,
-    routes: [GoRoute(path: HomeNavigationScreen.path, builder: (context, state) => const HomeNavigationScreen())],
+    routes: [
+      GoRoute(path: HomeNavigationScreen.path, builder: (context, state) => const HomeNavigationScreen()),
+      GoRoute(path: BrandsListScreen.path, builder: (context, state) => const BrandsListScreen()),
+    ],
   );
 });
