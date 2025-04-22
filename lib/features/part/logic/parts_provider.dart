620a395d887d342a3c1a2cb36d5b231a99c321bf
Khaleel Shaheen <khaleel_sh@hotmail.com>
2025-04-22 12:04:35 +0300

    Update parts data provider

--- lib/features/part/logic/parts_provider.dart
+++ lib/features/part/logic/parts_provider.dart
@@ -2,6 +2,10 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:summit_parts/features/part/data/parts_data_provider.dart';
 import 'package:summit_parts/features/part/model/part.dart';
 
-final featuredPartsProvider = FutureProvider<List<Part>>((ref) async {
+final featuredPartsProvider = FutureProvider.autoDispose<List<Part>>((ref) async {
   return ref.read(partsDataProvider).getFeaturedParts();
+});
+
+final partsProvider = FutureProvider.autoDispose<List<Part>>((ref) async {
+  return ref.read(partsDataProvider).getAllParts();
 });
