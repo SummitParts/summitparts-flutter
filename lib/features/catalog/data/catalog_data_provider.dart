0d5abde679be2c840e8391bac85a1a68d1683c84
Khaleel Shaheen <khaleel_sh@hotmail.com>
2025-04-25 17:28:29 +0300

    Update catalog data provider

--- lib/features/catalog/data/catalog_data_provider.dart
+++ lib/features/catalog/data/catalog_data_provider.dart
@@ -3,20 +3,17 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:summit_parts/core/data/http/dio_provider.dart';
 import 'package:summit_parts/features/catalog/model/catalog.dart';
 
-final categoriesDataProvider = Provider.autoDispose<CategoriesDataProvider>((ref) {
-  return CategoriesDataProvider(ref.read(dioProvider));
+final catalogDataProvider = Provider.autoDispose<CatalogDataProvider>((ref) {
+  return CatalogDataProvider(ref.read(dioProvider));
 });
 
-class CategoriesDataProvider {
-  CategoriesDataProvider(this._dio);
+class CatalogDataProvider {
+  CatalogDataProvider(this._dio);
 
   final Dio _dio;
 
   Future<Catalog> getCatalog(String id, {int page = 1, int limit = 10}) async {
-    final response = await _dio.get('/catalog/$id', queryParameters: {
-      'page': page,
-      'limit': limit,
-    });
+    final response = await _dio.get('/catalog/$id', queryParameters: {'page': page, 'limit': limit});
     return Catalog.fromJson(response.data);
   }
 }
