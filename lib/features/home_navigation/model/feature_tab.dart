792a38326870560430b38bdd5fb2adf507da6dd1
Khaleel Shaheen <khaleel_sh@hotmail.com>
2025-04-28 12:26:18 +0300

    Add specials category

--- lib/features/home_navigation/model/feature_tab.dart
+++ lib/features/home_navigation/model/feature_tab.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:summit_parts/features/catalog/ui/screen/catalog_screen.dart';
 import 'package:summit_parts/features/home/ui/screen/home_screen.dart';
 
 enum FeatureTab {
   dashboard(label: 'Home', icon: FontAwesomeIcons.house, screen: HomeScreen()),
+  specials(label: 'Specials', icon: FontAwesomeIcons.tags, screen: CatalogScreen(id: 'specials')),
   account(label: 'Account', icon: FontAwesomeIcons.userLarge, screen: Scaffold());
 
   const FeatureTab({required this.label, required this.icon, required this.screen});
