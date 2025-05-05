ad6d9a6c1eb3d57d0a917bbc3b942b061f009aa1
Khaleel Shaheen <khaleel_sh@hotmail.com>
2025-05-05 12:48:23 +0300

    Add auth and user providers

--- lib/features/auth/logic/auth_provider.dart
+++ lib/features/auth/logic/auth_provider.dart
@@ -1,17 +1,33 @@
 import 'dart:async';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:summit_parts/core/constants/storage_constants.dart';
+import 'package:summit_parts/core/data/local/in_memory_storage_data_provider.dart';
+import 'package:summit_parts/core/data/local/secure_storage_data_provider.dart';
 import 'package:summit_parts/features/auth/data/auth_data_provider.dart';
+import 'package:summit_parts/features/auth/logic/user_provider.dart';
 import 'package:summit_parts/features/auth/model/sign_in_response.dart';
 
 final authNotifierProvider = AsyncNotifierProvider.autoDispose<AuthNotifier, SignInResponse?>(AuthNotifier.new);
 
 class AuthNotifier extends AutoDisposeAsyncNotifier<SignInResponse?> {
+  late final AuthDataProvider _authDataProvider = ref.read(authDataProvider);
+  late final InMemoryStorageDataProvider _inMemoryStorageDataProvider = ref.read(inMemoryStorageProvider);
+  late final SecureStorageDataProvider _secureStorageDataProvider = ref.read(secureStorageProvider);
+
   @override
   Future<SignInResponse?> build() async => null;
 
   Future<void> signIn(String email, String password) async {
     state = AsyncValue.loading();
-    state = await AsyncValue.guard(() => ref.read(authDataProvider).signIn(email: email, password: password));
+    state = await AsyncValue.guard(() async {
+      final signInResponse = await _authDataProvider.signIn(email: email, password: password);
+      _inMemoryStorageDataProvider.put(key: StorageConstants.accessTokenKey, value: signInResponse.accessToken);
+      _inMemoryStorageDataProvider.put(key: StorageConstants.refreshTokenKey, value: signInResponse.refreshToken);
+      await _secureStorageDataProvider.write(key: StorageConstants.accessTokenKey, value: signInResponse.accessToken);
+      await _secureStorageDataProvider.write(key: StorageConstants.refreshTokenKey, value: signInResponse.refreshToken);
+      ref.invalidate(userNotifierProvider);
+      return signInResponse;
+    });
   }
 }
---
+++ lib/features/auth/logic/user_provider.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:summit_parts/core/constants/storage_constants.dart';
+import 'package:summit_parts/core/data/local/in_memory_storage_data_provider.dart';
+import 'package:summit_parts/core/data/local/secure_storage_data_provider.dart';
+import 'package:summit_parts/features/auth/model/sign_in_response.dart';
+
+final userNotifierProvider = AsyncNotifierProvider.autoDispose<UserNotifier, SignInResponse?>(UserNotifier.new);
+
+class UserNotifier extends AutoDisposeAsyncNotifier<SignInResponse?> {
+  late final InMemoryStorageDataProvider _inMemoryStorageDataProvider = ref.read(inMemoryStorageProvider);
+  late final SecureStorageDataProvider _secureStorageDataProvider = ref.read(secureStorageProvider);
+
+  @override
+  Future<SignInResponse?> build() async {
+    final accessToken = await _secureStorageDataProvider.read(key: StorageConstants.accessTokenKey);
+    final refreshToken = await _secureStorageDataProvider.read(key: StorageConstants.refreshTokenKey);
+    if (accessToken != null && refreshToken != null) {
+      _inMemoryStorageDataProvider.put(key: StorageConstants.accessTokenKey, value: accessToken);
+      _inMemoryStorageDataProvider.put(key: StorageConstants.refreshTokenKey, value: refreshToken);
+      return SignInResponse(accessToken: accessToken, refreshToken: refreshToken);
+    }
+    return null;
+  }
+
+  void signOut() {
+    _secureStorageDataProvider.delete(key: StorageConstants.accessTokenKey);
+    _secureStorageDataProvider.delete(key: StorageConstants.refreshTokenKey);
+    _inMemoryStorageDataProvider.delete(key: StorageConstants.accessTokenKey);
+    _inMemoryStorageDataProvider.delete(key: StorageConstants.refreshTokenKey);
+    state = AsyncData(null);
+  }
+}
