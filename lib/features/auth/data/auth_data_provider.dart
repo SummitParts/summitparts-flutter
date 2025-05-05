a8178bd8c5cab3ee16308cfa4a64576ab2626381
Khaleel Shaheen <khaleel_sh@hotmail.com>
2025-05-05 11:18:06 +0300

    Add auth data provider

---
+++ lib/features/auth/data/auth_data_provider.dart
@@ -0,0 +1,19 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:summit_parts/core/data/http/dio_provider.dart';
+import 'package:summit_parts/features/auth/model/sign_in_response.dart';
+
+final authDataProvider = Provider.autoDispose<AuthDataProvider>((ref) {
+  return AuthDataProvider(ref.read(dioProvider));
+});
+
+class AuthDataProvider {
+  const AuthDataProvider(this._dio);
+
+  final Dio _dio;
+
+  Future<SignInResponse> signIn({required String email, required String password}) async {
+    final response = await _dio.post('/auth/login', data: {'userID': email, 'password': password});
+    return SignInResponse.fromJson(response.data);
+  }
+}
