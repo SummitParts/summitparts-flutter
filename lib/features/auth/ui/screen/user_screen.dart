edc998642159de4e2b861da65b4ea638da935956
Khaleel Shaheen <khaleel_sh@hotmail.com>
2025-05-05 12:49:43 +0300

    Add user screen

---
+++ lib/features/auth/ui/screen/user_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:summit_parts/features/auth/logic/user_provider.dart';
+import 'package:summit_parts/features/auth/ui/screen/auth_screen.dart';
+
+class UserScreen extends ConsumerWidget {
+  const UserScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userAsync = ref.watch(userNotifierProvider);
+    return userAsync.maybeWhen(
+      data: (user) {
+        return user == null
+            ? AuthScreen()
+            : Scaffold(
+              appBar: AppBar(
+                title: const Text('User Login'),
+                backgroundColor: Theme.of(context).colorScheme.primary,
+                foregroundColor: Theme.of(context).colorScheme.onPrimary,
+              ),
+              body: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('User Logged In'),
+                    TextButton(
+                      onPressed: () {
+                        ref.read(userNotifierProvider.notifier).signOut();
+                      },
+                      child: Text('Logout'),
+                    ),
+                  ],
+                ),
+              ),
+            );
+      },
+      orElse: () => Scaffold(body: Center(child: CircularProgressIndicator.adaptive())),
+    );
+  }
+}
