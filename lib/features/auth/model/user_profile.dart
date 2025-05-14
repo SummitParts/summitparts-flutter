9f0ea2877a392112138e69b88a460c0b92e8d480
Khaleel Shaheen <khaleel_sh@hotmail.com>
2025-05-14 14:39:10 +0300

    Add user profile model

---
+++ lib/features/auth/model/user_profile.dart
@@ -0,0 +1,30 @@
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'user_profile.g.dart';
+
+@JsonSerializable()
+class UserProfile extends Equatable {
+  const UserProfile({
+    required this.userKey,
+    required this.userId,
+    required this.customerId,
+    required this.customerNo,
+    required this.arDivisionNo,
+    required this.customerKey,
+    required this.fullName,
+  });
+
+  final int userKey;
+  final String userId;
+  final String customerId;
+  final String customerNo;
+  final String arDivisionNo;
+  final int customerKey;
+  final String? fullName;
+
+  factory UserProfile.fromJson(Map<String, dynamic> json) => _$UserProfileFromJson(json);
+
+  @override
+  List<Object?> get props => [userKey, userId, customerId, customerNo, arDivisionNo, customerKey, fullName];
+}
---
+++ lib/features/auth/model/user_profile.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_profile.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+UserProfile _$UserProfileFromJson(Map<String, dynamic> json) => UserProfile(
+  userKey: (json['userKey'] as num).toInt(),
+  userId: json['userId'] as String,
+  customerId: json['customerId'] as String,
+  customerNo: json['customerNo'] as String,
+  arDivisionNo: json['arDivisionNo'] as String,
+  customerKey: (json['customerKey'] as num).toInt(),
+  fullName: json['fullName'] as String?,
+);
