5cb656e3119ec7ce92a774e9a1154caa80b22e0a
Khaleel Shaheen <khaleel_sh@hotmail.com>
2025-05-14 16:44:48 +0300

    Integrate /auth/refresh endpoint

--- lib/core/data/http/authorization_interceptor.dart
+++ lib/core/data/http/authorization_interceptor.dart
@@ -1,13 +1,19 @@
 import 'dart:io';
 
 import 'package:dio/dio.dart';
+import 'package:summit_parts/core/constants/api_constants.dart';
 import 'package:summit_parts/core/constants/storage_constants.dart';
 import 'package:summit_parts/core/data/local/in_memory_storage_data_provider.dart';
+import 'package:summit_parts/core/data/local/secure_storage_data_provider.dart';
+import 'package:summit_parts/features/auth/model/sign_in_response.dart';
 
 class AuthorizationInterceptor extends Interceptor {
-  AuthorizationInterceptor(this.inMemoryStorageDataProvider);
+  AuthorizationInterceptor(this.inMemoryStorageDataProvider, this.secureStorageDataProvider, this.dio);
 
   final InMemoryStorageDataProvider inMemoryStorageDataProvider;
+  final SecureStorageDataProvider secureStorageDataProvider;
+  final Dio dio;
+  bool _isRefreshing = false;
 
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
@@ -18,5 +24,57 @@ class AuthorizationInterceptor extends Interceptor {
       });
     }
     handler.next(options);
+  }
+
+  @override
+  Future<void> onError(DioException err, ErrorInterceptorHandler handler) async {
+    if (err.response?.statusCode == 401 && !_isRefreshing) {
+      _isRefreshing = true;
+      try {
+        // Get the refresh token
+        final refreshToken = inMemoryStorageDataProvider.get<String>(key: StorageConstants.refreshTokenKey);
+        if (refreshToken != null) {
+          // Call refresh token endpoint
+          // Create a base dio instance without interceptors for the auth interceptor to use
+          final refreshDio = Dio(BaseOptions(baseUrl: ApiConstants.baseUrl));
+
+          final response = await refreshDio.post(
+            '/auth/refresh',
+            options: Options(headers: {HttpHeaders.authorizationHeader: 'Bearer $refreshToken'}),
+          );
+
+          final signInResponse = SignInResponse.fromJson(response.data);
+          // Update tokens in storage
+          inMemoryStorageDataProvider.put(key: StorageConstants.accessTokenKey, value: signInResponse.accessToken);
+          inMemoryStorageDataProvider.put(key: StorageConstants.refreshTokenKey, value: signInResponse.refreshToken);
+          await secureStorageDataProvider.write(
+            key: StorageConstants.accessTokenKey,
+            value: signInResponse.accessToken,
+          );
+          await secureStorageDataProvider.write(
+            key: StorageConstants.refreshTokenKey,
+            value: signInResponse.refreshToken,
+          );
+
+          // Retry the original request
+          final options = err.requestOptions;
+          options.headers[HttpHeaders.authorizationHeader] = 'Bearer ${signInResponse.accessToken}';
+
+          final retryResponse = await dio.fetch(options);
+          handler.resolve(retryResponse);
+          _isRefreshing = false;
+          return;
+        }
+      } catch (e) {
+        // If refresh fails, clear tokens and let the error propagate
+        inMemoryStorageDataProvider.delete(key: StorageConstants.accessTokenKey);
+        inMemoryStorageDataProvider.delete(key: StorageConstants.refreshTokenKey);
+        secureStorageDataProvider.delete(key: StorageConstants.accessTokenKey);
+        secureStorageDataProvider.delete(key: StorageConstants.refreshTokenKey);
+      } finally {
+        _isRefreshing = false;
+      }
+    }
+    handler.next(err);
   }
 }
--- lib/core/data/http/dio_provider.dart
+++ lib/core/data/http/dio_provider.dart
@@ -4,6 +4,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:summit_parts/core/constants/api_constants.dart';
 import 'package:summit_parts/core/data/http/authorization_interceptor.dart';
 import 'package:summit_parts/core/data/local/in_memory_storage_data_provider.dart';
+import 'package:summit_parts/core/data/local/secure_storage_data_provider.dart';
 
 const _defaultTimeout = Duration(seconds: 30);
 
@@ -12,6 +13,11 @@ final dioProvider = Provider<Dio>((ref) {
     BaseOptions(baseUrl: ApiConstants.baseUrl, connectTimeout: _defaultTimeout, receiveTimeout: _defaultTimeout),
   );
   dio.transformer = BackgroundTransformer();
-  dio.interceptors.addAll([LoggyDioInterceptor(), AuthorizationInterceptor(ref.read(inMemoryStorageProvider))]);
+
+  dio.interceptors.addAll([
+    AuthorizationInterceptor(ref.read(inMemoryStorageProvider), ref.read(secureStorageProvider), dio),
+    LoggyDioInterceptor(),
+  ]);
+
   return dio;
 });
