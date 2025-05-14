c9d1d5e91ebf09b79bf49e39fa84724302990fbc
Khaleel Shaheen <khaleel_sh@hotmail.com>
2025-05-14 14:39:27 +0300

    Add getUserProfile request

--- lib/features/auth/data/auth_data_provider.dart
+++ lib/features/auth/data/auth_data_provider.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:summit_parts/core/data/http/dio_provider.dart';
 import 'package:summit_parts/features/auth/model/sign_in_response.dart';
+import 'package:summit_parts/features/auth/model/user_profile.dart';
 
 final authDataProvider = Provider.autoDispose<AuthDataProvider>((ref) {
   return AuthDataProvider(ref.read(dioProvider));
@@ -15,5 +16,10 @@ class AuthDataProvider {
   Future<SignInResponse> signIn({required String email, required String password}) async {
     final response = await _dio.post('/auth/login', data: {'userID': email, 'password': password});
     return SignInResponse.fromJson(response.data);
+  }
+
+  Future<UserProfile> getUserProfile() async {
+    final response = await _dio.get('/user/profile');
+    return UserProfile.fromJson(response.data);
   }
 }
