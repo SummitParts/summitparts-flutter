3808088a5d94bfaeab17517b983c2e1049499461
Khaleel Shaheen <khaleel_sh@hotmail.com>
2025-05-19 18:06:14 +0300

    Add ApiConstants.pageSize and set it to 10 by default

--- lib/features/catalog/data/catalog_data_provider.dart
+++ lib/features/catalog/data/catalog_data_provider.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:summit_parts/core/constants/api_constants.dart';
 import 'package:summit_parts/core/data/http/dio_provider.dart';
 import 'package:summit_parts/features/catalog/model/catalog.dart';
 
@@ -12,7 +13,7 @@ class CatalogDataProvider {
 
   final Dio _dio;
 
-  Future<Catalog> getCatalog(String id, {int page = 1, int limit = 10}) async {
+  Future<Catalog> getCatalog(String id, {int page = 1, int limit = ApiConstants.pageSize}) async {
     final response = await _dio.get('/catalog/$id', queryParameters: {'page': page, 'limit': limit});
     return Catalog.fromJson(response.data);
   }
