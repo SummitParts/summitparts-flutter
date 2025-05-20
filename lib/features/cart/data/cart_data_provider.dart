f0d462a675f7f0b42a9bf2c59c216863c478bbe8
Khaleel Shaheen <khaleel_sh@hotmail.com>
2025-05-20 16:59:58 +0300

    Add cartDataProvider initial setup

---
+++ lib/features/cart/data/cart_data_provider.dart
@@ -0,0 +1,36 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:summit_parts/core/data/http/dio_provider.dart';
+
+final cartDataProvider = Provider.autoDispose<CartDataProvider>((ref) {
+  return CartDataProvider(ref.read(dioProvider));
+});
+
+class CartDataProvider {
+  const CartDataProvider(this._dio);
+
+  final Dio _dio;
+
+  Future<dynamic> getCart() async {
+    final response = await _dio.get('/cart');
+    return response.data;
+  }
+
+  Future<dynamic> addToCart(String productId) async {
+    final response = await _dio.post(
+      '/cart',
+      data: {'comment': 'string', 'itemID': 'string', 'quantity': 0, 'unitOfMeasure': 'string', 'unitPrice': 0},
+    );
+    return response.data;
+  }
+
+  Future<dynamic> updateItemInCart(String cartDetailKey) async {
+    final response = await _dio.patch('/cart/$cartDetailKey', data: {'quantity': 0});
+    return response.data;
+  }
+
+  Future<dynamic> removeFromCart(String cartDetailKey) async {
+    final response = await _dio.delete('/cart/$cartDetailKey');
+    return response.data;
+  }
+}
