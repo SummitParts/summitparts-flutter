e1f215fb691f6b88713d12f270ae109ba4c2be94
Khaleel Shaheen <khaleel_sh@hotmail.com>
2025-05-20 13:20:16 +0300

    Add pagination to catalog provider

--- lib/features/catalog/logic/catalog_provider.dart
+++ lib/features/catalog/logic/catalog_provider.dart
@@ -2,6 +2,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:summit_parts/features/catalog/data/catalog_data_provider.dart';
 import 'package:summit_parts/features/catalog/model/catalog.dart';
 
-final catalogProvider = FutureProvider.autoDispose.family<Catalog, String>((ref, id) async {
-  return ref.read(catalogDataProvider).getCatalog(id);
-});
+typedef CatalogPage = ({String id, int pageIndex});
+
+final paginatedCatalogProvider = FutureProvider.autoDispose.family<Catalog, CatalogPage>(
+  (ref, catalogPage) async => ref.read(catalogDataProvider).getCatalog(catalogPage.id, page: catalogPage.pageIndex + 1),
+);
